29be71b0469a186ff1dec67ba7a37e1d4b03424d
guntur <gunturpdng@gmail.com>
2026-06-01 20:09:15 +0800

    feat(mvp): created the mvp program, where i can create the directory

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
 
+import Data.Time (defaultTimeLocale, formatTime, getCurrentTime, getCurrentTimeZone, utcToLocalTime)
+import Options.Applicative
+import System.Directory (createDirectoryIfMissing)
+
 newtype Options = Options {folderName :: String}
 
+optionsParser :: Parser Options
+optionsParser = Options <$> strArgument (metavar "FOLDER_NAME" <> help "The base name for the new directory")
+
+opts :: ParserInfo Options
+opts = info (optionsParser <**> helper) (fullDesc <> progDesc "Create a directory prefixed with today's date (YYYYMMDD-FOLDER_NAME)" <> header "dtmd - Date Time Make Directory")
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  options <- execParser opts
+  now <- getCurrentTime
+  tz <- getCurrentTimeZone
+
+  let localTime = utcToLocalTime tz now
+      dateStr = formatTime defaultTimeLocale "%Y%m%d" localTime
+      fullFolderName = dateStr ++ "-" ++ folderName options
+
+  createDirectoryIfMissing False fullFolderName
+  putStrLn $ "Created Directory: " ++ fullFolderName
